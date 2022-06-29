85db08ee440f8c81de4482cdf2d2b1b420647bf3
namnv3899 <nam.nv3899@gmail.com>
2022-06-29 22:22:04 +0700

    Namnv: update file readme

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # data_integration
 # run web
-1. Tạo database: test trong mongodb, import file data json
+1. Tạo database test trong mongodb,
 2. Vào thư mục web và chạy câu lệnh sau trong terminal:
   npm i,
   Sau đó
   npm run start:dev,
+  Sau khi chạy xong server, vào database test đã tạo trước đó và import file json vào bảng apps
   Sau khi chạy xong server thì run file html, để hiện thị dữ liệu thì ấn nút "Tìm kiếm" trên giao diện
--- web/src/app.module.ts
+++ web/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
-
+import { App, AppSchema } from './app.schema';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test')],
   controllers: [AppController],
+  imports: [
+    MongooseModule.forFeature([{ name: App.name, schema: AppSchema }]),
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
